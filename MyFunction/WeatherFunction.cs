afeadf42fe94aeb2b7f776e51b233c0d74e892f8
Primoz Stukelj <primoz.stukelj@inova.si>
2024-09-20 08:11:51 +0200

    Remvoed WeatherFuntion and defined new function file inside myfunction

---
+++ MyFunction/WeatherFunction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace MyFunction;
+
+public class WeatherFunction
+{
+    private readonly ILogger<WeatherFunction> _logger;
+
+    public WeatherFunction(ILogger<WeatherFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("WeatherFunction")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
